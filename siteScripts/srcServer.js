68fd32821d08cbe51b9ca33d96448a8eb62bf75b
Michael Rui <michael@rui.net.au>
2018-08-12 11:15:31 +1000

    Fixed the loading directory issue in express. had to mount the module directory.

--- siteScripts/srcServer.js
+++ siteScripts/srcServer.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var open = require('open');
-var phaser = require('phaser');
 
 var port = 3000;
 var app = express();
@@ -9,6 +8,7 @@ var app = express();
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
+app.use("/node_modules", express.static(__dirname + "/node_modules"));
 
 app.listen(port, function(err) {
     if (err) {
